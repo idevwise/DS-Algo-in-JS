a467c5de5e1fd7a75689d3985d20221c3013b48b
Rahul Kumar <rahulkumar.java@gmail.com>
2021-10-24 23:09:45 +0530

    Binary Search.

---
+++ BinarySearch.js
@@ -0,0 +1,17 @@
+function binarySearch(list, item) {
+    let low = 0, high = list.length-1, mid = 0;
+    while (low <= high) {
+        mid = Math.floor((low+high)/2);
+        if (list[mid] === item) {
+            return mid;
+        } else if (list[mid] < item) {
+            low = list[mid];
+        } else {
+            high = list[mid-1];
+        }
+    }
+    return -1;
+}
+
+//Test
+console.log(binarySearch([1,2,3,4,5,6,7,8,9], 7));
