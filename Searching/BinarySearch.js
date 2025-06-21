29fd11df267ada4e8ef1cecdd9290c173d6046b8
Rahul Kumar <idevwise@gmail.com>
2025-06-21 13:02:27 +0530

    Fix binary search index updates

--- Searching/BinarySearch.js
+++ Searching/BinarySearch.js
@@ -5,13 +5,13 @@ function binarySearch(list, item) {
         if (list[mid] === item) {
             return mid;
         } else if (list[mid] < item) {
-            low = list[mid];
+            low = mid + 1;
         } else {
-            high = list[mid-1];
+            high = mid - 1;
         }
     }
     return -1;
 }
 
-//Test
-console.log(binarySearch([1,2,3,4,5,6,7,8,9], 7));
+// Test
+console.log(binarySearch([1, 2, 3, 4, 5, 6, 7, 8, 9], 7));
